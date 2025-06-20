ab4eb4c9150e05ed66cd9ef8a3d560bf66ecc21f
jamesKamwela <121252480+jamesKamwela@users.noreply.github.com>
2025-06-20 09:27:18 +0300

    Add files via upload

--- Constants.cs
+++ Constants.cs
@@ -7,6 +7,8 @@ namespace ResearchApp
         // Database file names
         public const string EmployeesDatabaseFilename = "Employees.db3";
         public const string ClientsDatabaseFilename = "Clients.db3";
+        public const string WorkRecordDatabaseFilename = "WorkRecord.db3";
+        public const string SummeriesDatabaseFileName = "Summeries.db3";
 
         // Database paths
         public static string EmployeesDatabasePath =>
@@ -14,6 +16,12 @@ namespace ResearchApp
 
         public static string ClientsDatabasePath =>
             Path.Combine(FileSystem.AppDataDirectory, ClientsDatabaseFilename);
+
+        public static string WorkRecordDatabasePath =>
+            Path.Combine(FileSystem.AppDataDirectory, WorkRecordDatabaseFilename);
+
+        public static string SummeriesDatabasePath => 
+            Path.Combine(FileSystem.AppDataDirectory, SummeriesDatabaseFileName);
 
         // SQLite connection flags
         public const SQLiteOpenFlags Flags =
